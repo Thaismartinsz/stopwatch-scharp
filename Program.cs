bdf51f888d0a3cda29c9db9a70e41fafa24b3d63
Thais Martins <thaismtinocotm@gmail.com>
2022-04-15 19:41:19 -0300

    Menu

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿    using System.Threading;
+
+    Menu();
+
+    static void Menu() 
+    {
+        Console.Clear();
+        Console.WriteLine("S = Segundos");
+        Console.WriteLine("M = Minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar?");
+    }
+
+
+    static void Start(int time)
+    {
+        int currenttTime = 0;
+
+        while (currenttTime != time)
+        {
+            Console.Clear();
+            currenttTime++;
+            Console.WriteLine(currenttTime);
+            Thread.Sleep(1000);
+        }
+
+        Console.Clear();
+        Console.WriteLine("Stopwatch finalizado!");
+        Thread.Sleep(2500);
+    }
